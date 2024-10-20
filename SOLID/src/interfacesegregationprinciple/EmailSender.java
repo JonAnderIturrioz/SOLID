d8bded30a7654ccea400c56a8e634ee51e20427f
JonAnderIturrioz <jiturrioz08@gmail.com>
2024-10-20 21:29:22 +0200

    ISP parcialmente completado

---
+++ SOLID/src/interfacesegregationprinciple/EmailSender.java
@@ -0,0 +1,7 @@
+package interfacesegregationprinciple;
+
+public class EmailSender {
+	public static void sendEmail(EmailContact c, String message){
+		//Envía un mensaje a la dirección de correo electrónico del Contacto c.
+	}
+}
---
+++ SOLID/src/interfacesegregationprinciple/SMSSender.java
@@ -0,0 +1,7 @@
+package interfacesegregationprinciple;
+
+public class SMSSender {
+	public static void sendSMS(SMSContact c, String message){
+		//Envía un mensaje SMS al teléfono del Contacto c.
+	}
+}
